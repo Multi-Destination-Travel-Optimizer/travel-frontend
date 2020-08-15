d900b28727dee1afb493e4fbf3d7d1ea1b52b836
Isaac Tong <isaact943@outlook.com>
2020-08-15 12:28:47 +0800

    Integration of MapBox

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map from './components/Map'
+
+const App = () => {
+    return(
+        
+        <div>
+            <Map></Map>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+
+const MapComponent = ReactMapboxGl({
+    accessToken: process.env.REACT_APP_MAPBOX_KEY
+  });
+
+const Map = () => {
+    return (
+        <MapComponent
+  style="mapbox://styles/mapbox/streets-v9"
+  containerStyle={{
+    height: '100vh',
+    width: '100vw'
+  }}
+>
+  <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+    <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+  </Layer>
+</MapComponent>
+    );
+};
+
+export default Map;
