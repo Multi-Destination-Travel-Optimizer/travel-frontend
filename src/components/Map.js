5fdaaf9755ffd43fe71c1b66a68f47ec17837b1d
Tong, Isaac <isaact2@illinois.edu>
2020-08-19 10:28:47 -0700

    Event is passed as second argument of callback. First argument might be a DOM click event

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,23 +2,27 @@ import React from "react";
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 
 const MapComponent = ReactMapboxGl({
-    accessToken: process.env.REACT_APP_MAPBOX_KEY
-  });
+  accessToken: process.env.REACT_APP_MAPBOX_KEY
+});
 
 const Map = () => {
-    return (
-        <MapComponent
-  style="mapbox://styles/mapbox/streets-v9"
-  containerStyle={{
-    height: '100vh',
-    width: '100vw'
-  }}
->
-  <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-    <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-  </Layer>
-</MapComponent>
-    );
+  return (
+    <MapComponent
+      style="mapbox://styles/mapbox/streets-v9"
+      containerStyle={{
+        height: '100vh',
+        width: '100vw'
+      }}
+      //Event is passed as the second argument of the callback function
+      onClick={(_, event) => {
+        console.log(event)
+      }}
+    >
+      <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+      </Layer>
+    </MapComponent>
+  );
 };
 
 export default Map;
