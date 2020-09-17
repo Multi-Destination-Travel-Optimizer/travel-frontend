6fb3512708b8235f41807adaf2f916594628597e
Isaac Tong <isaact943@outlook.com>
2020-09-17 18:17:47 -0500

    added concept user interface

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+### CONCEPT USER INTERFACE
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,23 +1,49 @@
 import React from "react";
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
 const MapComponent = ReactMapboxGl({
-    accessToken: process.env.REACT_APP_MAPBOX_KEY
-  });
+    accessToken: process.env.REACT_APP_MAPBOX_KEY,
+    logoPosition: "top-left",
+    renderWorldCopies: true,
+    dragRotate: false,
+});
 
 const Map = () => {
     return (
         <MapComponent
-  style="mapbox://styles/mapbox/streets-v9"
-  containerStyle={{
-    height: '100vh',
-    width: '100vw'
-  }}
->
-  <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-    <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-  </Layer>
-</MapComponent>
+            style="mapbox://styles/mapbox/streets-v9"
+            containerStyle={{
+                height: "100vh",
+                width: "100vw",
+            }}
+            onClick={(event) => {
+                console.log(event);
+            }}
+        >
+            <Layer
+                type="symbol"
+                id="marker"
+                layout={{ "icon-image": "harbor-15" }}
+            >
+                <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+            </Layer>
+            <Layer
+                type="symbol"
+                id="marker"
+                layout={{ "icon-image": "harbor-15" }}
+            >
+                <Feature
+                    coordinates={[-10.481747846041145, 100.3233379650232]}
+                />
+            </Layer>
+            <Layer
+                type="symbol"
+                id="marker"
+                layout={{ "icon-image": "harbor-15" }}
+            >
+                <Feature coordinates={[50.481747846041145, 50.3233379650232]} />
+            </Layer>
+        </MapComponent>
     );
 };
 
